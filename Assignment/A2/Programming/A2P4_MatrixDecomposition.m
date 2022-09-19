71ef83b7c9679e4dc8cf3d2e6ba0f49d11dd2c04
mikami520 <yxiao39@jh.edu>
2022-09-19 18:32:40 -0400

    update for A2

--- Assignment/A2/Programming/A2P4_MatrixDecomposition.m
+++ Assignment/A2/Programming/A2P4_MatrixDecomposition.m
@@ -4,7 +4,10 @@ clear all
 %% 4.1 QR Decomposition test
 
 A = randn(5, 4)
-[Q_package, R_package] = qr(A)
+[Q_package, R_package] = qr(A, 'econ')
 [Q_mine, R_mine] = QR_Decomposition(A)
 
 %% 4.2 LU Decomposition test
+B = magic(5)*100
+[L_package, U_package] = lu(B)
+[L_mine, U_mine] = LU_Decomposition(B)
--- Assignment/A2/Programming/LU_Decomposition.m
+++ Assignment/A2/Programming/LU_Decomposition.m
@@ -1,3 +1,23 @@
 function [L, U] = LU_Decomposition(A)
-    L = zeros(3,4);
+    assert(size(A, 1) == size(A, 2))
+    assert(det(A) ~= 0)
+    L = zeros(size(A));
+    U = zeros(size(A));
+    [L, U] = next(A, 1, L, U, size(A, 1));
+end
+
+function [L, U] = next(A, i, L, U, n)
+    beta = A(i, i);
+    x = A(i, i+1:n);
+    v = A(i+1:n, i);
+    w = v / beta;
+    L(i,i) = 1;
+    U(i,i) = beta;
+    U(i, i+1:n) = x;
+    L(i+1:n, i) = w;
+    %L_star = 
+    if i == n
+        return
+    end
+    [L, U] = next(A, i+1, L, U, n);
 end
