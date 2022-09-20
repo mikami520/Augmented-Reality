8c00e5421e45f98bf6c2496b92ff470c88d4fccc
mikami520 <yxiao39@jh.edu>
2022-09-20 17:25:32 -0400

    update for A2

--- Assignment/A2/Programming/A2P4_MatrixDecomposition.m
+++ Assignment/A2/Programming/A2P4_MatrixDecomposition.m
@@ -6,17 +6,18 @@ clear all
 A = randn(5, 4);
 [Q_package, R_package] = qr(A, 'econ');
 [Q_mine, R_mine] = QR_Decomposition(A);
-if Q_mine*R_mine - A < 0.005 * min(abs(A), abs(Q_mine*R_mine))
+if Q_mine*R_mine - A < 0.0005 * min(abs(A), abs(Q_mine*R_mine))
     disp('Correct RQ implementation')
 else
     disp('Incorrect RQ implementation')
 end
 %% 4.2 LU Decomposition test
-B = magic(6);
-[L_package, U_package, P_package] = lu(B);
-[L_mine, U_mine] = LU_Decomposition(B);
-
-if L_mine*U_mine - B < 0.005 * min(abs(B), abs(L_mine*U_mine))
+B = randn(6);
+[~, ~, P_package] = lu(B);
+C = P_package * B;
+[L_package, U_package] = lu(C)
+[L_mine, U_mine] = LU_Decomposition(C)
+if L_mine*U_mine - C < 0.0005 * min(abs(C), abs(L_mine*U_mine))
     disp('Correct LU implementation')
 else
     disp('Incorrect LU implementation')
