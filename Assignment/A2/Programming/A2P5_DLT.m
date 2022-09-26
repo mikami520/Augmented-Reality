4772dcb1458bcd6ee578ab27c8faa2e29fc11629
mikami520 <yxiao39@jh.edu>
2022-09-26 14:18:24 -0400

    update for A2P5

--- Assignment/A2/Programming/A2P5_DLT.m
+++ Assignment/A2/Programming/A2P5_DLT.m
@@ -29,7 +29,34 @@ end
 [d, ind] = sort(diag(D));
 h_ATA = V(:, ind(1));
 H_ATA = reshape(h_ATA', [3, 3])';
+
 %% Problem 5(b)
 [U, S, V] = svd(A);
 h_SVD = V(:, size(V, 2));
 H_SVD = reshape(h_SVD', [3, 3])';
+
+
+%% Verification of problem 5(a)
+for i=1:length(X)
+    x = X(i, :);
+    x_prime_ATA = H_ATA * [x, 1]';
+    x_prime_ATA = [x_prime_ATA(1) x_prime_ATA(2)] ./ x_prime_ATA(3);
+    % compute the norm of difference between estimated position and true position
+    if norm(x_prime_ATA - X_dot(i, :)) < 5e-15
+        fprintf('test H_ATA on point %d is correct !\n', i);
+    else
+        fprintf('test H_ATA on point %d is wrong !\n', i);
+    end
+end
+%% Verification of problem 5(b)
+for i=1:length(X)
+    x = X(i, :);
+    x_prime_SVD = H_SVD * [x, 1]';
+    x_prime_SVD = [x_prime_SVD(1) x_prime_SVD(2)] ./ x_prime_SVD(3);
+    % compute the norm of difference between estimated position and true position
+    if norm(x_prime_SVD - X_dot(i, :)) < 5e-15
+        fprintf('test H_SVD on point %d is correct !\n', i);
+    else
+        fprintf('test H_SVD on point %d is wrong !\n', i);
+    end
+end
